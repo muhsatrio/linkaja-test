15c8363f1a03e7ffa3aceefa06319175acf1599e
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2021-09-01 20:59:55 +0700

    fix: wrong way on compare bcrypt

--- platform/bcrypt/bcrypt.go
+++ platform/bcrypt/bcrypt.go
@@ -3,12 +3,8 @@ package bcrypt
 import "golang.org/x/crypto/bcrypt"
 
 func Compare(reqPassword, password string, saltHash int) (err error) {
-	hashedReqPass, err := bcrypt.GenerateFromPassword([]byte(reqPassword), saltHash)
-	if err != nil {
-		return
-	}
 
-	err = bcrypt.CompareHashAndPassword(hashedReqPass, []byte(password))
+	err = bcrypt.CompareHashAndPassword([]byte(password), []byte(reqPassword))
 
 	return
 }
